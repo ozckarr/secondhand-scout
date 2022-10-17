ff6f1bd45c7eb81125265a899a79f729d94362a4
Oskar Lindblad <lindbladoskar@gmail.com>
2022-10-17 12:26:18 +0200

    Re-commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { Typography } from "@material-ui/core";
+//import { useUser } from "../context/AuthContext";
+
+export default function Home() {
+  return <Typography variant="h1">Hello</Typography>;
+}
